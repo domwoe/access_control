0e73d56f7baad72e7e0fb23d44c994b5dbe4a8da
Dominic Woerner <dominic.woerner@dfinity.org>
2023-01-23 15:12:24 +0100

    fix minor bug
    Signed-off-by: Dominic Woerner <dominic.woerner@dfinity.org>

--- demo.sh
+++ demo.sh
@@ -8,7 +8,7 @@ AUTHZ_CAN_ID=$(dfx canister id authz_canister)
 
 # Need to set root key of local replica as second argument if used locally
 ROOT_KEY=$(dfx ping | sed -n 's/.*"root_key": \(.*\)/\1/p')
-dfx deploy resource_canister --argument "(principal \"$AUTHZ_CAN_ID\", opt blob \"$ROOT_KEY\"T)"
+dfx deploy resource_canister --argument "(principal \"$AUTHZ_CAN_ID\", opt blob \"$ROOT_KEY\")"
 
 MY_ID=$(dfx identity get-principal)
 echo 'Setting permissions for '$MY_ID 'to use GET and INC but not SET...'
